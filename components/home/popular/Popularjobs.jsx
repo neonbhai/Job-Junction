fe7e8e65c719957758e2646dadd65ae6419fee65
Someshwar Tripathi <someshwartripathi8@gmail.com>
2023-05-17 20:39:48 +0530

    useFetch.js defined

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,42 @@
+import { useRouter } from "expo-router";
+
+import React from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+
+const Popularjobs = () => {
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={["1", "2", "3", "4"]}
+            renderItem={({ item }) => <PopularJobCard />}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/neon.jpg";
+
+export default {
+  profile,
+};
