f09ff844435a93e4c2ff508b7080b37f327ba87a
Someshwar Tripathi <someshwartripathi8@gmail.com>
2023-06-06 13:19:15 +0530

    new pattern experiment (commented) (Changes nothing)

--- utils/index.js
+++ utils/index.js
@@ -2,6 +2,7 @@ export const checkImageURL = (url) => {
     if (!url) return false;
     else {
         const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
+        // const pattern = new RegExp('neon.jpg | (png|jpg|jpeg|bmp|gif|webp)$', 'i');
         return pattern.test(url);
     }
 }
